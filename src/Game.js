77dcadfd9d67eea0a3e6c0e3efcbeacbb0c76977
salvoab <salvatore.abate85@gmail.com>
2021-05-20 12:12:51 +0200

    logica di base del componente Game

--- src/Game.js
+++ src/Game.js
@@ -43,4 +43,120 @@ export default class Game extends Lightning.Component {
       },
     }
   }
+
+  // Lifecycle event _construct
+  _construct() {
+    // current player tile index
+    this._index = 0
+
+    // computer score
+    this._aiScore = 0
+
+    // player score
+    this._playerScore = 0
+  }
+
+  // The lifecycle event active will be called when a component visible property is true,
+  // alpha higher then 0 and positioned in the renderable screen.
+  _active() {
+    this._reset()
+
+    // we iterate over the outlines of the field and do a nice
+    // transition of the width / height, so it looks like the
+    // lines are being drawn realtime.
+
+    this.tag('Field').children.forEach((el, idx) => {
+      el.setSmooth(idx < 2 ? 'w' : 'h', 900, { duration: 0.7, delay: idx * 0.15 })
+    })
+  }
+
+  // Metodi
+
+  _reset() {
+    // reset tiles
+    this._tiles = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
+
+    // force render
+    this.render(this._tiles)
+
+    // change back to rootstate
+    this._setState('')
+  }
+
+  render(tiles) {
+    this.tag('Markers').children = tiles.map((el, idx) => {
+      return {
+        x: (idx % 3) * 300 + 110,
+        // Nota: ~~ equivale ad un Math.floor() per numeri positivi
+        y: ~~(idx / 3) * 300 + 90,
+        text: { text: el === 'e' ? '' : `${el}`, fontSize: 100 },
+      }
+    })
+  }
+
+  // On remotecontrol up
+  _handleUp() {
+    let idx = this._index
+    if (idx - 3 >= 0) {
+      this._setIndex(idx - 3)
+    }
+  }
+
+  // On remotecontrol down
+  _handleDown() {
+    let idx = this._index
+    if (idx + 3 <= this._tiles.length - 1) {
+      this._setIndex(idx + 3)
+    }
+  }
+
+  // On remotecontrol left
+  _handleLeft() {
+    const idx = this._index
+    if (idx % 3) {
+      this._setIndex(idx - 1)
+    }
+  }
+
+  // On remotecontrol right
+  _handleRight() {
+    const newIndex = this._index + 1
+    if (newIndex % 3) {
+      this._setIndex(newIndex)
+    }
+  }
+
+  // Metodo
+  _setIndex(idx) {
+    this.tag('PlayerPosition').patch({
+      smooth: {
+        x: (idx % 3) * 300 + 425,
+        y: ~~(idx / 3) * 300 + 125,
+      },
+    })
+    this._index = idx
+  }
+
+  // On remotecontrol enter
+  _handleEnter() {
+    if (this._tiles[this._index] === 'e') {
+      if (this.place(this._index, 'X')) {
+        this._setState('Computer')
+      }
+    }
+  }
+
+  //Metodo
+  place(index, marker) {
+    this._tiles[index] = marker
+    this.render(this._tiles)
+
+    const winner = Utils.getWinner(this._tiles)
+    if (winner) {
+      this._setState('End.Winner', [{ winner }])
+      return false
+    }
+
+    return true
+  }
 }
